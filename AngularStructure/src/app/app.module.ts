4f9cc5b936799f83fe2d97f7e60161171e066692
Riya222 <riyajofi97@gmail.com>
2022-06-07 23:45:53 +0530

    designed login page and implemented login with credentials set from UI
    Need to integrate BE service, this is just a dummy login. And also need to implement guard service too

--- AngularStructure/src/app/app.module.ts
+++ AngularStructure/src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +23,18 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularStructure/src/app/components/login/login.component.ts
+++ AngularStructure/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthServiceService } from 'src/app/auth-service.service';
+import { Router, CanActivate } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  formGroup: any =  FormGroup;
 
-  constructor() { }
+  constructor(private authServide: AuthServiceService, public router: Router) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+  initForm(){
+    this.formGroup = new FormGroup({
+      email: new FormControl('',[Validators.required]),
+      password: new FormControl('',[Validators.required]),
+    });
+  }
+
+  loginProcess(){
+    if (this.formGroup.valid){
+      console.log(this.formGroup.value);
+      if (this.formGroup.value['email'] == 'Dayal' && this.formGroup.value['password'] == 'riya'){
+        console.log("success");
+        this.router.navigate(['dashboard']);
+      }
+      else {
+        console.log("failed");
+      }
+    }
   }
 
 }
